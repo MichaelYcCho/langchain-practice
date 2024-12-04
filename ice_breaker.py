f5798ca7458ae0d5cbd7ff63e1f3acc48fe83459
youngchan-itall <michael.yc.cho@gmail.com>
2024-12-04 23:05:58 +0900

    feat: PromptTemplate Test

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,17 +1,28 @@
 from dotenv import load_dotenv
 import os
 
+from langchain.chains.qa_with_sources.stuff_prompt import template
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
+def get_summary_content(information):
+    return  f"""
+    given the information {information} about a person from I want you to create:
+    1. a short summary
+    2. two interesting facts about them
+    """
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     load_dotenv()
-    print(os.getenv("DB_HOST"))
-    print_hi("PyCharm")
+    summary_template = get_summary_content("a person")
+    summary_prompt_template = PromptTemplate(input_variables="information", template=summary_template)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4-mini")
+
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={
+        "information": "a person"
+    })
