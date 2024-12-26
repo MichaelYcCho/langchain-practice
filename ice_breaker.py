ecf8056cf3a36cb1e4640127a875ce7f59766f2f
youngchan-itall <michael.yc.cho@gmail.com>
2024-12-26 23:43:23 +0900

    feat: OutputParser

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,28 +3,49 @@ import os
 
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+
 from langchain_core.prompts import PromptTemplate
 
+from output_parser import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from third_parties.twitter import scrape_user_tweets
 
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, mock=True)
 
     summary_template = """
-    given the Linkedin information {information} about a person I want you to create:
+    given the information about a person from linkedin {information},
+    and their latest twitter posts {twitter_posts} I want you to create:
     1. A short summary
-    2. two interesting facts about them
+    2. two interesting facts about them 
+
+    Use both information from twitter and Linkedin
+    \n{format_instructions}
     """
+
+    # get_format_instructions()은 pydanitc 객체를 가져와 스키마를 추출하고
+
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = summary_prompt_template | llm
+    #chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
