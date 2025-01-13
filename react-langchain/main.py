fc724b64d1e24138378df460c2f30b44da9bae41
youngchan-itall <michael.yc.cho@gmail.com>
2025-01-13 23:59:09 +0900

    feat: React_agant Base

---
+++ react-langchain/main.py
@@ -0,0 +1,27 @@
+from typing import Union, List
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import Tool, tool
+from langchain.tools.render import render_text_description
+
+
+load_dotenv()
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
+
+    return len(text)
+
+if __name__ == "__main__":
+    print("Hello ReAct LangChain!")
+    tools = [get_text_length]
+
