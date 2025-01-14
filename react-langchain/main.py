0c30bd5d3d41da5851dc5bd1ec4d31d60eec5d06
youngchan-itall <michael.yc.cho@gmail.com>
2025-01-14 23:09:00 +0900

    feat: ReAct Agent Tool 정의

--- react-langchain/main.py
+++ react-langchain/main.py
@@ -7,7 +7,6 @@ from langchain.schema import AgentAction, AgentFinish
 from langchain.tools import Tool, tool
 from langchain.tools.render import render_text_description
 
-
 load_dotenv()
 
 
@@ -21,7 +20,46 @@ def get_text_length(text: str) -> int:
 
     return len(text)
 
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
+
+
 if __name__ == "__main__":
     print("Hello ReAct LangChain!")
     tools = [get_text_length]
 
+     # from https://smith.langchain.com/hub/hwchase17/react
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:
+    """
+
+    # partial() 메서드는 템플릿의 변수를 채우는 데 사용됩니다. 어떤 도구를 사용할지 또는 도구의 설명을 채우는 데 사용
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
